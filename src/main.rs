de73e099b28aa6773ffe3376860e1200f8a0d275
Mika Naylor <mail@autophagy.io>
2023-01-28 23:03:26 +0100

    Setup basic bot, with a slap command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,148 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+use std::env;
+
+use serenity::async_trait;
+use serenity::framework::standard::macros::{command, group, help, hook};
+use serenity::framework::standard::{
+    help_commands, Args, CommandGroup, CommandResult, DispatchError, HelpOptions, StandardFramework,
+};
+use serenity::http::Http;
+use serenity::model::channel::Message;
+use serenity::model::gateway::{GatewayIntents, Ready};
+use serenity::model::id::UserId;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("Connected as {}", ready.user.name);
+    }
+}
+
+#[group]
+#[commands(slap)]
+struct General;
+
+#[help]
+#[individual_command_tip = "Wes þu hal.\n\n\
+For help about a specific command, pass it as an argument to ``!help``.\n"]
+#[command_not_found_text = "Could not find: `{}`."]
+#[max_levenshtein_distance(3)]
+#[strikethrough_commands_tip_in_guild = ""]
+async fn help(
+    context: &Context,
+    msg: &Message,
+    args: Args,
+    help_options: &'static HelpOptions,
+    groups: &[&'static CommandGroup],
+    owners: HashSet<UserId>,
+) -> CommandResult {
+    let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
+    Ok(())
+}
+
+#[hook]
+async fn before(_: &Context, msg: &Message, command_name: &str) -> bool {
+    println!(
+        "Got command '{}' by user '{}'",
+        command_name, msg.author.name
+    );
+    true
+}
+
+#[hook]
+async fn after(_: &Context, _msg: &Message, command_name: &str, command_result: CommandResult) {
+    match command_result {
+        Ok(()) => println!("Processed command '{}'", command_name),
+        Err(why) => println!("Command '{}' returned error {:?}", command_name, why),
+    }
+}
+
+#[hook]
+async fn unknown_command(_ctx: &Context, _msg: &Message, unknown_command_name: &str) {
+    println!("Could not find command named '{}'", unknown_command_name);
+}
+
+#[hook]
+async fn dispatch_error(ctx: &Context, msg: &Message, error: DispatchError, _command_name: &str) {
+    if let DispatchError::Ratelimited(info) = error {
+        if info.is_first_try {
+            let _ = msg
+                .channel_id
+                .say(
+                    &ctx.http,
+                    &format!("Try this again in {} seconds.", info.as_secs()),
+                )
+                .await;
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let http = Http::new(&token);
+
+    let (owners, _) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.with_whitespace(true).prefix("!").owners(owners))
+        .before(before)
+        .after(after)
+        .unrecognised_command(unknown_command)
+        .on_dispatch_error(dispatch_error)
+        .help(&HELP)
+        .group(&GENERAL_GROUP);
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
+
+#[command]
+#[description = "Attack your target with a 🐟"]
+async fn slap(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let target = if args.is_empty() {
+        format!("<@{}>", &msg.author.id)
+    } else {
+        args.rest().to_string()
+    };
+
+    msg.channel_id
+        .say(
+            &ctx.http,
+            format!(
+                "{} slaps {} around a bit with a large trout",
+                &ctx.cache.current_user().name,
+                target
+            ),
+        )
+        .await?;
+    Ok(())
 }
